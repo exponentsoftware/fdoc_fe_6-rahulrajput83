774165bf126985eb628d0dbd3bc4c6338964b507
rahulrajput83 <rahulrajput21112@gmail.com>
2022-10-14 16:15:50 +0530

    Point 2 Completed

--- album/src/App.js
+++ album/src/App.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 import Album from './components/Album';
 
 function App() {
+  const [data, setData] = useState({
+    image: 'https://upload.wikimedia.org/wikipedia/en/2/2b/Rockstar_%28soundtrack%29.jpg',
+    album: 'Rockstar',
+    artist: 'A.R. Rahman',
+  })
   return (
-    <Album />
+    <Album data={data} />
   );
 }
 
--- album/src/components/Album.jsx
+++ album/src/components/Album.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import './Album.css'
 
 
-function Album() {
+function Album(props) {
     return (
         <div className='card'>
-            <img src='https://upload.wikimedia.org/wikipedia/en/2/2b/Rockstar_%28soundtrack%29.jpg' alt='' />
-            <span className='album-name'>Rockstar</span>
-            <span className='musician-name'>A.R. Rahman</span>
+            <img src={props.data.image} alt='' />
+            <span className='album-name'>{props.data.album}</span>
+            <span className='musician-name'>{props.data.artist}</span>
         </div>
     )
 }
